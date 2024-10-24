d6e13530ccaca18af768745a7879ce7dd26b0033
aasimohyeah <aasimohyeah@gmail.com>
2024-10-24 14:50:53 +0530

    86

--- Natours/server.js
+++ Natours/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config.env' }); //loads variables from a .env file to process.env
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -17,12 +17,47 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DB connection successful');
   });
 
-const port = process.env.PORT || 3000; // process.env.PORT defined in config.env
+// Mongoose Schema (its like a blueprint/class for the model)
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Error: Missing name'],
+    unique: true, //instruction for name string to be unique
+  },
+  rating: {
+    type: Number,
+    default: 4.5, //default value
+  },
+  price: {
+    type: Number,
+    required: [true, 'Error: Missing price'],
+  },
+});
+// Mongoose model
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The forest hiker',
+  price: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error: ', err);
+  });
+
+const port = process.env.PORT || 3000; //process.env.PORT defined in config.env
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
