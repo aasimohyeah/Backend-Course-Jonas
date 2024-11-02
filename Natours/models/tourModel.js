b4ab4181f2c6183889ac56e12430c63b0e7d7b3c
aasimohyeah <aasimohyeah@gmail.com>
2024-11-02 23:31:50 +0530

    94

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Error: Missing name'],
     unique: true, //instruction for name string to be unique
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Error: Missing duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Error: Specify group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Error: Specify difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5, //default value
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0, //default value
   },
   price: {
     type: Number,
     required: [true, 'Error: Missing price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, //for whitespace trim
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Error: Missing description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Error: Missing cover image'],
+  },
+  images: [String], //an array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 // Mongoose model
 const Tour = mongoose.model('Tour', tourSchema);
