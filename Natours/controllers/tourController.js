d909404f59177ef3d13670353359c7b753f0eaf0
aasimohyeah <aasimohyeah@gmail.com>
2024-11-13 18:33:06 +0530

    116

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel.js');
 const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync.js');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -8,9 +9,8 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    /*
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  /*
     console.log(req.query); // shows all parameters included in the URL
     
     //BUILD QUERY
@@ -60,199 +60,161 @@ exports.getAllTours = async (req, res) => {
       if (skip >= numTours) throw new Error('This page does not exist');
     }*/
 
-    //EXECUTE QUERY
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const tours = await features.query;
+  //EXECUTE QUERY
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const tours = await features.query;
 
-    //SEND RESPONSE
-    res.status(200).json({
-      //jsend format used below
-      status: 'success',
-      results: tours.length, //additional field
-      data: {
-        tours: tours, //2nd tours is tours variable above, 1st tours is from url /api/v1/tours
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  //SEND RESPONSE
+  res.status(200).json({
+    //jsend format used below
+    status: 'success',
+    results: tours.length, //additional field
+    data: {
+      tours: tours, //2nd tours is tours variable above, 1st tours is from url /api/v1/tours
+    },
+  });
+});
 
-exports.getTour = async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   /*
   console.log(req.params);
   const id = req.params.id * 1; //convert string to number by multiplying with 1
   console.log(id);
   const tour = tours.find((el) => el.id === id);*/
-  try {
-    const tour = await Tour.findById(req.params.id);
-    res.status(200).json({
-      //jsend format used below
-      status: 'success',
-      data: {
-        tours: tour,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
 
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body); //req.body will contain data that came as response from the post request
+  const tour = await Tour.findById(req.params.id);
+  res.status(200).json({
+    //jsend format used below
+    status: 'success',
+    data: {
+      tours: tour,
+    },
+  });
+});
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+//next() is needed below in the parameters, so that error can be sent to the globalErrorHandler using it
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body); //req.body will contain data that came as response from the post request
 
-exports.updateTour = async (req, res) => {
-  try {
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: tour, //2nd one is tour variable from above
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
 
-exports.deleteTour = async (req, res) => {
-  try {
-    await Tour.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  // try {
+  // } catch (err) {
+  //   res.status(400).json({
+  //     status: 'fail',
+  //     message: err,
+  //   });
+  // }
+});
+
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour, //2nd one is tour variable from above
+    },
+  });
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  await Tour.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 //MongoDB Aggregation pipelines can be used to get insights from our data, eg: avg, total, max, min etc
-exports.getTourStats = async (req, res) => {
-  try {
-    const stats = await Tour.aggregate([
-      {
-        $match: { ratingsAverage: { $gte: 4.5 } },
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: { $toUpper: '$difficulty' }, //to sort below values acc to difficulty
+        numTours: { $sum: 1 }, //increment by 1 for every entry that go through pipeline
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
       },
-      {
-        $group: {
-          _id: { $toUpper: '$difficulty' }, //to sort below values acc to difficulty
-          numTours: { $sum: 1 }, //increment by 1 for every entry that go through pipeline
-          numRatings: { $sum: '$ratingsQuantity' },
-          avgRating: { $avg: '$ratingsAverage' },
-          avgPrice: { $avg: '$price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' },
+    },
+    {
+      $sort: { avgPrice: 1 }, //1 here means ascending order
+    },
+    // {
+    //   $match: { _id: { $ne: 'easy' } },
+    // },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats: stats, //2nd one is stats variable from above
+    },
+  });
+});
+
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1;
+
+  const plan = await Tour.aggregate([
+    {
+      $unwind: '$startDates',
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
         },
       },
-      {
-        $sort: { avgPrice: 1 }, //1 here means ascending order
+    },
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: '$name' },
       },
-      // {
-      //   $match: { _id: { $ne: 'easy' } },
-      // },
-    ]);
+    },
+    {
+      $addFields: { month: '$_id' },
+    },
+    {
+      $project: {
+        _id: 0, //0 means dont show _id
+      },
+    },
+    {
+      $sort: { numTourStarts: -1 }, //-1 means descending
+    },
+    {
+      $limit: 12,
+    },
+  ]);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats: stats, //2nd one is stats variable from above
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const year = req.params.year * 1;
-
-    const plan = await Tour.aggregate([
-      {
-        $unwind: '$startDates',
-      },
-      {
-        $match: {
-          startDates: {
-            $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-31`),
-          },
-        },
-      },
-      {
-        $group: {
-          _id: { $month: '$startDates' },
-          numTourStarts: { $sum: 1 },
-          tours: { $push: '$name' },
-        },
-      },
-      {
-        $addFields: { month: '$_id' },
-      },
-      {
-        $project: {
-          _id: 0, //0 means dont show _id
-        },
-      },
-      {
-        $sort: { numTourStarts: -1 }, //-1 means descending
-      },
-      {
-        $limit: 12,
-      },
-    ]);
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        plan, //2nd one is plan variable from above
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      plan, //2nd one is plan variable from above
+    },
+  });
+});
 
 // export.<fn_name> is used because multiple functions are being exported
 // module.exports is used when only one function is exported
