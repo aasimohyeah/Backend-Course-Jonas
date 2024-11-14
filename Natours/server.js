113fed75d178a18d3a3e8a2bcbd5ede972c04b50
aasimohyeah <aasimohyeah@gmail.com>
2024-11-14 19:36:53 +0530

    123

--- Natours/server.js
+++ Natours/server.js
@@ -4,6 +4,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION. Shutting down application...');
+  console.log(err.name, err.message);
+  process.exit(1); //0-success, 1-uncalled exception
+});
+
 dotenv.config({ path: './config.env' }); //loads variables from a .env file to process.env
 
 const app = require('./app');
@@ -25,6 +31,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3000; //process.env.PORT defined in config.env
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION. Shutting down application...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1); //0-success, 1-uncalled exception
+  });
 });
