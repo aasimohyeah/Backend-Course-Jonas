219889974420049749d32d7531bb6cd6746fb735
aasimohyeah <aasimohyeah@gmail.com>
2024-11-19 13:46:27 +0530

    146

--- Natours/app.js
+++ Natours/app.js
@@ -3,18 +3,60 @@ const express = require('express');
 
 const app = express();
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError.js');
 const globalErrorHandler = require('./controllers/errorController.js');
 const tourRouter = require('./routes/tourRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
 
-//1.middleware
+//1.global middlewares
+
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`)); //serving static files from public folder
+
+//Limit requests from the same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, try again in 1 hour',
+});
+app.use('/api', limiter);
+
+//Body parser: it reads data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+//Serving static files from public folder
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Custom Middleware');
