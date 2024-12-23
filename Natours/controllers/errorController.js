e35d062cd8b38c0f1aabd861d957dafcfadfa053
aasimohyeah <aasimohyeah@gmail.com>
2024-12-24 02:58:05 +0530

    193

--- Natours/controllers/errorController.js
+++ Natours/controllers/errorController.js
@@ -26,34 +26,63 @@ const handleJWTExpiredError = (err) =>
   new AppError('Token expired. Please log in again', 401);
 //^no need to type return above cuz ES6 feature
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+  //API
+  // req.originalUrl returns the url minus the host
+  if (req.originalUrl.startsWith('./api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+  //RENDERED WEBSITE
+  console.error('ERROR', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went Wrong',
+    msg: err.message,
   });
 };
 
-const sendErrorProd = (err, res) => {
-  //Operational error, trusted: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  }
-  //Programming error or other unknown error: dont show error details to client
-  else {
+const sendErrorProd = (err, req, res) => {
+  //1) API
+  if (req.originalUrl.startsWith('/api')) {
+    //Operational error, trusted: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    //Programming error or other unknown error: dont show error details to client
     //1.Log error
     console.error('ERROR', err);
 
     //2.Send generic message
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
-      message: 'Something went wrong!',
+      message: '1Something went wrong!',
     });
   }
+
+  //2) RENDERED WEBSITE
+  //Operational error, trusted: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: '2Something went Wrong!',
+      msg: err.message,
+    });
+  }
+  //Programming error or other unknown error: dont show error details to client
+  //1.Log error
+  console.error('ERROR', err);
+
+  //2.Send generic message
+  return res.status(err.statusCode).render('error', {
+    title: '3Something went Wrong!',
+    msg: 'Please try again later',
+  });
 };
 
 //GLOBAL ERROR HANDLING MIDDLEWARE HAS 4 PARAMETERS:
@@ -65,11 +94,12 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   //console.log('XXX', err.name);
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     //console.log('production yes');
     let error = { ...err }; //... means node destructuring: i.e unpacking all values in an array
     //console.log('YYY', error);
+    error.message = err.message;
 
     //Handling some errors below which have defined names. eg:CastError
     if (err.name === 'CastError') {
@@ -88,6 +118,6 @@ module.exports = (err, req, res, next) => {
       error = handleJWTExpiredError(error);
     }
 
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
